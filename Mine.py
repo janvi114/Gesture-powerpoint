bea86c95d89d87e6229412e0e7dcf30eaee0e1a6
Janvi Dubey <dubeyjahanvi114@gmail.com>
2025-02-02 12:49:36 +0530

    Remove Mine.py from repository

--- Mine.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-iris_data=pd.read_csv("D:\ALL DOWNLOADS\Study material\iris.data.csv")
-fig=plt.gcf()
-fig.set_size_inches(10,7)
-fig=sns.boxplot(x="Species",y="SepalLengthCm",data=iris_data)
-
-plt.show()
